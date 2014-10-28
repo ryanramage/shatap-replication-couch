683de6271e8d857def1558388abd6fbd0a5b10b4
Ryan Ramage <ryan.ramage@gmail.com>
2014-10-28 10:13:29 -0600

    minor tweeks

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ var couchr = require('couchr');
 
 var couch = process.argv[2];
 
+if (!couch) return console.log('please provide a couchdb url');
+
 var db = url.resolve(couch, '_replicator');
 var all_docs = url.resolve(db +'/', '_all_docs');
+
+console.log('deleting replication docs from', db)
 
 couchr.get(all_docs, function(err, resp){
   if (err) return console.log(err);
@@ -23,7 +27,7 @@ couchr.get(all_docs, function(err, resp){
 
 
 function rm(item, cb){
-  if (item.id.indexOf('_design') === 0) return; // skip design docs
+  if (item.id.indexOf('_design') === 0) return cb(); // skip design docs
   var doc_url = url.resolve(db + '/', item.id);
   couchr.del(doc_url, {rev: item.value.rev}, function(err, resp){
     if (err) return cb(err);
